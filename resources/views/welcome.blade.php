3b5a64a72362cebbd496860a751de1db49e3eb0e
silvio di gregorio <silviofrancescodigregorio98gmail.com>
2024-01-15 20:59:45 +0100

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,13 @@
             </div>
         </div>
     </header>
-    
+
     {{-- silvia mi dice di mettere un commento --}}
     {{-- aggiungo il commento serio, bisogna sistemare la rotta delle categorie --}}
-    
+
 
             <div class="d-flex justify-content-around">
-                <div class="col-lg-12 mb-5 mt-5 p-5" >
+                <div class="col-sm-4 col-md-6 col-lg-8 col-xl-10 col-xxl-12  mb-5 mt-5 p-5" >
                     <!-- xl circle buttons-->
                     <a href="/categories/1"><button class="btn btn-success btn-circle btn-circle-xl"><i class="fa-solid fa-book"></i></button></a>
                     <a href="/categories/2"><button class="btn btn-primary btn-circle btn-circle-xl"><i class="fa-solid fa-music"></i></button></a>
